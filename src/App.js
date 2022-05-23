dd410d79d44221ce68b032b8820ecc03eaccd06d
MofuMofu <froakie002@gmail.com>
2022-05-23 22:20:12 +0900

    feature: p25 コンポーネント作成
    テストを書くときは分割したい気持ちがわかった気がする

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ToDoItem(props) {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" checked={props.item.done} />
+        {props.item.name}
+      </label>
+    </li>
+  )
+}
+
+function TodoList(props) {
+  return (
+    <ul className='todo-list'>
+      {props.items.map((item) => (
+        <ToDoItem key={item.id} item={item} />
+      ))}
+    </ul>
+  )
+}
+
+function App(props) {
+  const items = [
+    {id:1, name: "リングフィットアドベンチャー", done: false},
+    {id:2, name: "散歩", done: true},
+    {id:3, name: "サイクリング", done: false},
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todoモック</h1>
+        <TodoList items={items} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
